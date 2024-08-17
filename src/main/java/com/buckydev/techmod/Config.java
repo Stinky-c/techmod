58c8b9b4f5e8c9ce3b19450a540dda0bc1d44726
Bucky <60587749+Stinky-c@users.noreply.github.com>
2024-08-16 20:21:02 -0700

    Add missing gradle wrapper

--- src/main/java/com/buckydev/techmod/Config.java
+++ src/main/java/com/buckydev/techmod/Config.java
@@ -16,14 +16,18 @@ import net.neoforged.neoforge.common.ModConfigSpec;
 // Demonstrates how to use Neo's config APIs
 @EventBusSubscriber(modid = TechMod.MODID, bus = EventBusSubscriber.Bus.MOD)
 public class Config {
+
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
     public static final ModConfigSpec.ConfigValue<String> MAGIC_NUMBER_INTRODUCTION = BUILDER.comment(
                     "What you want the introduction message to be for the magic number")
             .define("magicNumberIntroduction", "The magic number is... ");
+
     private static final ModConfigSpec.BooleanValue LOG_DIRT_BLOCK =
             BUILDER.comment("Whether to log the dirt block on common setup").define("logDirtBlock", true);
+
     private static final ModConfigSpec.IntValue MAGIC_NUMBER =
             BUILDER.comment("A magic number").defineInRange("magicNumber", 42, 0, Integer.MAX_VALUE);
+
     // a list of strings that are treated as resource locations for items
     private static final ModConfigSpec.ConfigValue<List<? extends String>> ITEM_STRINGS = BUILDER.comment(
                     "A list of items to log on common setup.")
