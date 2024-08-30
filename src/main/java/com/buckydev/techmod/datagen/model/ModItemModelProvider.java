e11a9ade46de012d7a9c7b334c6e9b9b8b158bf5
Bucky <60587749+Stinky-c@users.noreply.github.com>
2024-08-30 14:13:10 -0700

    Fix block entity models, and introduce json formatting

--- src/main/java/com/buckydev/techmod/datagen/model/ModItemModelProvider.java
+++ src/main/java/com/buckydev/techmod/datagen/model/ModItemModelProvider.java
@@ -4,6 +4,8 @@ import com.buckydev.techmod.TechMod;
 import com.buckydev.techmod.blocks.ModBlocks;
 import com.buckydev.techmod.items.ModItems;
 import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.ItemLike;
 import net.neoforged.neoforge.client.model.generators.ItemModelProvider;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 
@@ -16,15 +18,15 @@ public class ModItemModelProvider extends ItemModelProvider {
     @Override
     protected void registerModels() {
 
-        // It expects a string as the first argument for some reason
-        withExistingParent(ModBlocks.EXAMPLE_BLOCK.asItem().toString(), modLoc("block/example_block"));
-        withExistingParent(
-                ModBlocks.EXAMPLE_BE.asItem().toString(),
-                modLoc("block/example_be")); // FIXME: renders the block item wrong
-
-        withExistingParent(ModBlocks.SEC_MACHINE.asItem().toString(), modLoc("block/secondmachine"));
+        basicBlockItem(ModBlocks.EXAMPLE_BLOCK.asItem(), modLoc("block/example_block"));
+        basicBlockItem(ModBlocks.EXAMPLE_BE.asItem(), modLoc("block/example_be"));
+        basicBlockItem(ModBlocks.SEC_MACHINE.asItem(), modLoc("block/secondmachine"));
 
         basicItem(ModItems.EXAMPLE_FOOD.get());
         basicItem(ModItems.EXAMPLE_ITEM.get());
+    }
+
+    private void basicBlockItem(ItemLike item, ResourceLocation rl) {
+        withExistingParent(item.toString(), rl);
     }
 }
